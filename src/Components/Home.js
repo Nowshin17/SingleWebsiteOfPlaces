f40b9410c748d3468f84c0f97b5acddab9c891cd
nowshin2 <nowshin@algosolver.com>
2022-01-31 03:07:16 +0600

    Create Home.js

---
+++ src/Components/Home.js
@@ -0,0 +1,85 @@
+import React from 'react';
+
+export default function Home() {
+  return( 
+  <div>
+    
+  <div class=" flex">
+    <div class=" absolute top-0 pb-20 box-content h-50 w-full border-4 rounded-b-[30px]">
+      <h1 className= " pt-10 text-[50px] pl-20 text-[#334587] font-bold">
+          Want to Add new place?</h1>
+    {/* <button class="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-4 px-5 border border-blue-700 rounded-[12px]">
+          + add new places
+      </button>   */}
+    </div>
+  </div>
+
+
+
+
+<div class="absolute top-60 box-content bg-white h-full w-full border-4 rounded-[10px]">
+    <div class="flex flex-row">
+        <div><h1 className= "p-10 text-[30px] text-[#3a4570] font-bold">Popular Places</h1> </div>
+        <div className= " ">  <hr></hr> </div>  
+    </div>
+
+
+
+
+
+  <div class=" pl-10 flex flex-row ... ">
+
+      <div class=" basis-1/4 pt-40 absolute max-w-sm rounded overflow-hidden shadow-lg border-2 rounded-[20px] ">
+    
+          <div class="px-6 py-2  ">
+            <div class="font-bold text-xl mb-2">মানিকগঞ্জ </div>
+            <p class="text-orange-700 text-base font-bold"> হরিরামপুর  </p>
+          </div>
+
+          <div class="px-4 pt-4 pb-2">
+            <span class="inline-block text-[50px] text-center text-[#5e73db] bg-[#bbc5f9] rounded-[10px] px-40 py-3 text-sm font-semibold text-gray-700 mr-2 mb-2">EDIT</span>
+          </div>
+
+          
+      </div>
+
+
+
+      <div class=" basis-1/4 pt-40 absolute max-w-sm rounded overflow-hidden shadow-lg border-2 rounded-[20px] ">
+      
+          <div class="px-6 py-2 ">
+            <div class="font-bold text-xl mb-2">মানিকগঞ্জ </div>
+            <p class="text-orange-700 text-base font-bold"> হরিরামপুর </p>
+          </div>
+
+          <div class="px-4 pt-4 pb-2">
+            <span class="inline-block text-[50px] text-center text-[#5e73db] bg-[#bbc5f9] rounded-[10px] px-40 py-3 text-sm font-semibold text-gray-700 mr-2 mb-2">EDIT</span>
+          </div>
+
+    
+    </div>
+  
+  {/* <div class="flex flex-row ">
+    <div>1</div>
+    <div>2</div>
+    <div>3</div>
+  </div> */}
+
+</div>
+
+
+
+
+</div>
+
+
+ {/* <div  className= "bg-slate-300 h-full w-full">
+    <div class="shadow-inner ... h-32 w-32 p-12 border-4">fff</div>
+   
+    </div> */}
+
+</div>
+  )
+}
+
+<script src="../path/to/@themesberg/flowbite/dist/flowbite.bundle.js"></script>;
